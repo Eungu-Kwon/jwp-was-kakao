17a7176eabd9701e6a42b43ea22169665ef09978
Eungu-Kwon <kiwi9084@gmail.com>
2024-04-16 17:47:33 +0900

    feature: 템플릿 엔진을 활용하여 사용자 목록 페이지 추가

--- src/main/java/webserver/GetRequestHandler.java
+++ src/main/java/webserver/GetRequestHandler.java
@@ -3,10 +3,23 @@ package webserver;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
+import db.DataBase;
+import model.User;
 import webserver.constants.HttpHeaders;
 import webserver.constants.HttpStatus;
 
@@ -28,9 +41,12 @@ public class GetRequestHandler implements MethodRequestHandler {
         return Optional.empty();
     }
 
-    private static Optional<HttpResponse> responseResources(String path) throws IOException {
+    private Optional<HttpResponse> responseResources(String path) throws IOException {
         if (path.equals("/")) {
             path = "/index.html";
+        }
+        if (path.startsWith("/user/list")) {
+            return responseDynamicResource(path);
         }
         File file = new File(
             "./src/main/resources" + (path.endsWith(".html") || path.endsWith("favicon.ico")
@@ -46,5 +62,25 @@ public class GetRequestHandler implements MethodRequestHandler {
             return Optional.of(httpResponse);
         }
         return Optional.empty();
+    }
+
+    private Optional<HttpResponse> responseDynamicResource(String path) throws IOException {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+        handlebars.registerHelper("plus", new Helper<Integer>() {
+            @Override
+            public Object apply(Integer context, Options options) throws IOException {
+                return context + 1;
+            }
+        });
+        Template template = handlebars.compile("user/list");
+
+        Map<String, List<User>> users = new HashMap<>();
+		List<User> userData = new ArrayList<>(DataBase.findAll());
+        users.put("users", userData);
+        return Optional.of(new HttpResponse(HttpStatus.OK, Map.of(HttpHeaders.CONTENT_TYPE, "text/html"),
+            template.apply(users).getBytes()));
     }
 }
